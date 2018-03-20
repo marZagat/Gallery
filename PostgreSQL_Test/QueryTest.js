c9f5ce2b42bf6640ab5b058218acd4b488273a57
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2018-03-20 15:27:27 -0700

    added query test script to test time to search for a single id in all the tables

---
+++ PostgreSQL_Test/QueryTest.js
@@ -0,0 +1,47 @@
+const pgp = require('pg-promise')();
+const pg = require('pg');
+
+const db = pgp({
+  database: 'postgres',
+  port: 5432
+});
+
+const dbt = pgp({
+  database: 'restaurants',
+  port: 5432
+});
+
+//query function
+let querying = (id) => {
+  dbt.any(`SELECT * FROM restaurant WHERE id = 5848498;`).then((data) => {
+    console.log(data);
+  });
+  dbt.any(`SELECT * FROM photos WHERE id = 5848498;`).then((data) => {
+    console.log(data);
+  });
+  dbt.any(`SELECT * FROM users WHERE id = 5848498;`).then((data) => {
+    console.log(data);
+  });
+  dbt.any(`SELECT * FROM avatars WHERE id = 5848498;`).then((data) => {
+    console.log(data);
+  });
+  dbt.any(`SELECT * FROM dimensions WHERE id = 5848498;`).then((data) => {
+    console.log(data);
+  });
+}
+
+//async function which will log a start time and and end time when the async call is completed
+let testQueryTimes = async() => {
+  let start = new Date().getTime();
+  console.log(start);
+  let end;
+  let time;
+  for (let i = 0; i < 1; i++) {
+    await querying();
+    end = new Date().getTime();
+    time = end - start;
+  }
+  console.log(`Time in milliseconds: ${time}m/s`);
+}
+
+testQueryTimes();
