f2c1339b61c6555e199bad59b8c2e3087a4adcc1
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2018-03-24 13:48:55 -0700

    added clusters to server code

--- server/index.js
+++ server/index.js
@@ -16,12 +16,12 @@ MongoClient.connect('mongodb://localhost:27017');
 const Photos = require('../database/index.js');
 
 //cluster setup
-if(cluster.isMaster) {
-  var numWorkers = require('os').cpus().length;
+if (cluster.isMaster) {
+  let numWorkers = require('os').cpus().length;
 
   console.log('Master cluster setting up ' + numWorkers + ' workers...');
 
-  for(var i = 0; i < numWorkers; i++) {
+  for (let i = 0; i < numWorkers; i++) {
       cluster.fork();
   }
 
@@ -35,60 +35,51 @@ if(cluster.isMaster) {
       cluster.fork();
   });
 } else {
-  var app = require('express')();
-  app.all('/*', (req, res) => {
-    res.send('process ' + process.pid + ' says hello!').end();
-  })
+  const app = express();
 
-  var server = app.listen(8000, () => {
-      console.log('Process ' + process.pid + ' is listening to all incoming requests');
+  const client = redis.createClient(6379);
+
+  app.use(cors());
+
+  app.use(bodyParser.json());
+
+  // serve static files from dist dir
+  app.use('/restaurants/:id', express.static(path.join(__dirname, '../client/dist')));
+
+  //retrieve data using redis if it exists
+  const cache = (req, res, next) => {
+    let id = req.params.id;
+    client.get(id, (err, data) => {
+      if (err) throw err;
+
+      if (data != null) {
+        let parsedData = JSON.parse(data);
+        res.status(200).send(parsedData);
+      } else {
+        next();
+      }
+    });
+  }
+
+  //redis middlesware
+  app.get('/api/restaurants/:id/gallery', cache);
+
+  //retrieve data using new database
+  app.get('/api/restaurants/:id/gallery', (req, res) => {
+    let id = req.params.id;
+
+    utility.fetchOne(id, (err, data) => {
+      if (err) {
+        console.log(`encountered internal server error`);
+        res.status(500);
+      } else {
+        client.setex(id, 5000, JSON.stringify(data));
+        res.status(200).send(data);
+      }
+    });
   });
+
+  app.listen(2222, () => console.log('Gallery App listening on port 2222!'));
 }
 
-const app = express();
-
-const client = redis.createClient(6379);
-
-app.use(cors());
-
-app.use(bodyParser.json());
-
-// serve static files from dist dir
-app.use('/restaurants/:id', express.static(path.join(__dirname, '../client/dist')));
-
-//retrieve data using redis if it exists
-const cache = (req, res, next) => {
-  let id = req.params.id;
-  client.get(id, (err, data) => {
-    if (err) throw err;
-
-    if (data != null) {
-      let parsedData = JSON.parse(data);
-      res.status(200).send(parsedData);
-    } else {
-      next();
-    }
-  });
-}
-
-//redis middlesware
-app.get('/api/restaurants/:id/gallery', cache);
-
-//retrieve data using new database
-app.get('/api/restaurants/:id/gallery', (req, res) => {
-  let id = req.params.id;
-
-  utility.fetchOne(id, (err, data) => {
-    if (err) {
-      console.log(`encountered internal server error`);
-      res.status(500);
-    } else {
-      client.setex(id, 5000, JSON.stringify(data));
-      res.status(200).send(data);
-    }
-  });
-});
-
-app.listen(2222, () => console.log('Gallery App listening on port 2222!'));
-
-module.exports = app;
+// module.exports = app;
