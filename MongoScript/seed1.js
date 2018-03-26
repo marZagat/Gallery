7352a8fdfd1094bd7042d56d3ff8dcbdcda7e4ed
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2018-03-26 11:28:00 -0700

    cleaned up seeding files

--- MongoScript/seed1.js
+++ MongoScript/seed1.js
@@ -4,49 +4,34 @@ const faker = require('faker');
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017";
 
-// mongoose.connect('mongodb://localhost/photos');
-
 MongoClient.connect(url, function(err, client, db) {
   if (err) throw err;
   db = client.db('photos');
 
-//create a promise for the generation
-let items = (id) => {
-  return new Promise((resolve, reject) => {
-    let data = helper.generate(id);
-    db.collection("photos").insertMany(data, function(err, res) {
-      data = [];
-      if (err) throw err;
-      console.log("1 iteration inserted");
-      resolve();
+  //create a promise for the generation
+  let items = (id) => {
+    return new Promise((resolve, reject) => {
+      let data = helper.generate(id);
+      db.collection("photos").insertMany(data, function(err, res) {
+        data = [];
+        if (err) throw err;
+        console.log("1 iteration inserted");
+        resolve();
+      });
     });
-  });
-}
-let id_gen = 0;
-let create = async() => {
-  for (let i = 0; i < 334; i++) {
-    await items(id_gen);
-    console.log(`done with ${i}`);
-    id_gen = id_gen + 10000;
-    console.log(id_gen);
   }
-  client.close();
-}
+  let id_gen = 0;
+  let create = async() => {
+    for (let i = 0; i < 334; i++) {
+      await items(id_gen);
+      console.log(`done with ${i}`);
+      id_gen = id_gen + 10000;
+      console.log(id_gen);
+    }
+    client.close();
+  }
 
-create();
+  create();
 
-// let addIndex = async () => {
-//   await create();
-//   db.collection('photos').createIndex({'place_id': 1});
-//   console.log('added indexes');
-// }
-
-// addIndex();
-
-console.log('done seeding');
-// let closeConnection = async () => {
-//   await create();
-//   client.close();
-// }
-// closeConnection();
+  console.log('done seeding');
 });
